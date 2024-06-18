9de6701ed2d09bc1e6dc50563f124493969c63ce
suyog <140448121+xspatrian@users.noreply.github.com>
2024-06-18 20:17:24 +0530

    Add files via upload

---
+++ python/operators/operator_1.py
@@ -0,0 +1,11 @@
+print(10 + 5)
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+
---
+++ python/operators/operator_2.py
@@ -0,0 +1,4 @@
+Example
+Addition + and subtraction - has the same precedence, and therefor we evaluate the expression from left to right:
+
+print(5 + 4 - 7 + 3)
